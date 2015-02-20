3dc9ff6d51ad1837eaa8750d0de614d8f3fad7ff
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-02-20 13:47:15 -0800

    calculate number of slices

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
 var Pizza = {
+  toppings: "",
   size: function(newSize) {
-    this.size = newSize + "in";
+    this.size = newSize;
+  },
+  sliceAmount: function(c) {
+    return this.sliceAmount = this.size / 2 + " slices";
   }
 };
+
+
+
+
+
+
+// ---------------------------------------------------------
+
+// if (this.size <= 9) {
+//   return "2 slices";
+// } else if (this.size > 9 && < 15) {
+//   return "4 slices";
+// } else if (this.size >=15) {
+//
+// } else {
+//   return "something is wrong"
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -3,7 +3,15 @@ describe("Pizza", function() {
     it("takes diameter input and returns that input +in", function() {
     var testPizza = Object.create(Pizza);
     testPizza.size(10);
-    expect(testPizza.size).to.equal("10in");
+    expect(testPizza.size).to.equal(10);
+    });
+  });
+
+  describe("slices", function() {
+    it("takes an input of 10 and returns 5", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.size(10);
+      expect(testPizza.sliceAmount()).to.equal("5 slices");
     });
   });
 });
